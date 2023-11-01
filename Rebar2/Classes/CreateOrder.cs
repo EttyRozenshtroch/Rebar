14d7c5a2fe8b5902d4354e615f8e7c61b8f9e891
etty <etty4423@gmail.com>
2023-11-01 18:25:46 +0200

    add create order file

---
+++ Rebar2/Classes/CreateOrder.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rebar2.Classes
+{
+    internal static class CreateOrder
+    {
+        public static CreateOrder()
+        {
+            MenueDataAccess access = new MenueDataAccess();
+            var shakes = access.GetAllShakes().To;
+            foreach ( var shake in shakes )
+            {
+                Console.WriteLine(shake.id)+"\n";
+            }
+            Console.WriteLine("please enter which shake do you whant");
+            string nameShake=Console.ReadLine();
+            Shake shake = access.GetShakeById(nameShake);
+
+        }
+    }
+}
